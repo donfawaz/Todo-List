aecacf70fb68941c93fdd8a5419bc3799a588bc9
Fawaz <fawazalqaoud@gmail.com>
2019-05-18 18:06:48 +0300

    First Todo list

---
+++ testWeb/main.js
@@ -0,0 +1,32 @@
+let textField = document.querySelector("#textField")
+let Button = document.querySelector("#taskButton")
+let taskList = document.querySelector("#taskList")
+
+Button.onclick = function() {
+    let newTask = document.createElement('LI')
+    let text = textField.value
+    let textNode = document.createTextNode(text)
+
+    let checkbox = document.createElement('input')
+    checkbox.type = 'checkbox'
+
+
+    /*
+    * mainpulate CSS attribute at run time
+    */
+    checkbox.onclick = function() {
+        newTask.setAttribute('style', 'text-decoration: line-through')
+        newTask.setAttribute('style', 'opacity: 0')
+
+        window.setTimeout(function() {
+            taskList.removeChild(newTask)
+
+        }, 2000) 
+    }
+    
+    newTask.appendChild(checkbox)
+    newTask.appendChild(textNode)
+    taskList.appendChild(newTask)
+
+    textField.value = ''
+}
